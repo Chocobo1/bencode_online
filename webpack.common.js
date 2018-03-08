883d0d5793cf92c0dcb39dda377bb799ee165829
Chocobo1 <Chocobo1@users.noreply.github.com>
2018-03-08 12:24:52 +0800

    Update to webpack 4

--- webpack.common.js
+++ webpack.common.js
@@ -18,7 +18,7 @@ module.exports =
   ],
   output:
   {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,4 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
-module.exports = merge(common, {
-  plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true
-    })
-  ]
-});
+module.exports = merge(common);
