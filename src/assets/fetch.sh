f16f7ff3f93b43dfb1c5780e14c6a1766e753f38
Chocobo1 <Chocobo1@users.noreply.github.com>
2023-05-30 15:40:04 +0800

    Update bootstrap to 5.2.3
    And also fetch simple-icons from source repo.

--- src/assets/fetch.sh
+++ src/assets/fetch.sh
@@ -2,6 +2,7 @@
 
 wget \
   -P "$(dirname "$0")" \
-  https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css \
-  https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js \
-  https://cdn.jsdelivr.net/npm/simple-icons@9.0.0/icons/github.svg
+  --backups=1 \
+  https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css \
+  https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js \
+  https://raw.githubusercontent.com/simple-icons/simple-icons/9.0.0/icons/github.svg
