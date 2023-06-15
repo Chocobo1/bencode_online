e44afc99eec5a51050ad1df77fbcefeb50d5a7b7
Chocobo1 <Chocobo1@users.noreply.github.com>
2023-06-15 18:21:32 +0800

    Update dependencies

--- src/assets/fetch.sh
+++ src/assets/fetch.sh
@@ -5,4 +5,4 @@ wget \
   --backups=1 \
   https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css \
   https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js \
-  https://raw.githubusercontent.com/simple-icons/simple-icons/9.0.0/icons/github.svg
+  https://raw.githubusercontent.com/simple-icons/simple-icons/9.1.0/icons/github.svg
