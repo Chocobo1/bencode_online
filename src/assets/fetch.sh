6bc2de545c3417ca361e51ff04a1dbe577ced63b
Chocobo1 <Chocobo1@users.noreply.github.com>
2023-12-04 04:43:25 +0800

    Use parallel download

--- src/assets/fetch.sh
+++ src/assets/fetch.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-wget \
-  -P "$(dirname "$0")" \
-  --backups=1 \
-  https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css \
-  https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js \
-  https://raw.githubusercontent.com/simple-icons/simple-icons/9.9.0/icons/github.svg
+curl \
+  -L \
+  -Z \
+  --output-dir "$(dirname "$0")" \
+  -O https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css \
+  -O https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js \
+  -O https://raw.githubusercontent.com/simple-icons/simple-icons/9.9.0/icons/github.svg
